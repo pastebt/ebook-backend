897e9dbabbc12e035b5daf2497309a95d6f81f1f
pastebt <maye1968@gmail.com>
2012-10-10 16:51:36 -0700

    modified:   README.md 	modified:   dropbox.py

--- src/CloudStorage/Dropbox/README.md
+++ src/CloudStorage/Dropbox/README.md
@@ -6,4 +6,14 @@ dropbox [option path]
 	--upload, -U filename                       Upload local file to Dropbox cloud storage service
 	--delete, -D filename                        Delete the file in Dropbox cloud storage service
 	--query, -Q keyword-----------------Query the file by keyword in Dropbox cloud storage service
-	
+
+
+Need use --auth to get authorise from dropbox user, save key and sec
+Then next time this user can use this key/sec pair to access
+
+Depenence:
+    download dropbox sdk:
+    https://www.dropbox.com/static/developers/dropbox-python-sdk-1.5.1.zip
+
+    unzip and go into path:
+    python setup.py install
--- src/CloudStorage/Dropbox/dropbox.py
+++ src/CloudStorage/Dropbox/dropbox.py
@@ -17,9 +17,8 @@ ACCESS_TYPE = 'app_folder'
 
 class DropBoxClient(object):
     def __init__(self, key, sec):
+        # key and sec are got from auth
         sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
-        #key: '5k9obasoquq0qhv'
-        #sec: '3av8u8gcvepe547'
         sess.set_token(key, sec)
         self.client = client.DropboxClient(sess)
         #print "linked account:", client.account_info()
@@ -30,22 +29,22 @@ class DropBoxClient(object):
         url = sess.build_authorize_url(request_token)
         # Make the user sign in and authorize this token
         print "url:", url
-        print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
+        print "Please visit this website and press the",
+        print "'Allow' button, then hit 'Enter' here."
         raw_input()
-        try:    
-            # This will fail if the user didn't visit the above URL and hit 'Allow'
+        try:
+            # will fail if the user didn't visit the above URL and hit 'Allow'
             access_token = sess.obtain_access_token(request_token)
             print "please save these for the account"
             print "key:", repr(access_token.key)
             print "sec:", repr(access_token.secret)
-            
+
             client = client.DropboxClient(sess)
             print "linked account:", client.account_info()
         except Exception:
             print >> sys.stderr, "Failed to get access token"
 
     def meta(self, filename):
-        #f, metadata = client.get_file_and_metadata('/magnum-opus.txt',rev='362e2029684fe')
         f, metadata = self.client.get_file_and_metadata('/magnum-opus.txt')
         out = open('magnum-opus.txt', 'w')
         out.write(f.read())
@@ -78,7 +77,7 @@ def main():
     group.add_argument("--upload", "-U", help="Upload a file")
     group.add_argument("--delete", "-D", help="Delete a file")
     group.add_argument("--query", "-Q", help="Query file by key word")
-    
+
     args = parser.parse_args()
 
     if args.upload or args.delete or args.query or args.auth:
